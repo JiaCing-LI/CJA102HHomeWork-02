89db96b916eab839b4da9e33a2c254ec9de38c4d
Wa Li <orz30315@gmail.com>
2025-06-27 20:56:34 +0800

    fixed Hw2 and fixed Hw1-4

--- src/hw1/Hw1_4.java
+++ src/hw1/Hw1_4.java
@@ -1,6 +1,7 @@
 package hw1;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 //請定義一個常數為3.1415(圓周率)，並計算半徑為5的圓面積與圓周長
 public class Hw1_4 {
@@ -16,11 +17,14 @@ public class Hw1_4 {
 		BigDecimal radiusBD = new BigDecimal(radius);
 		BigDecimal piBD = new BigDecimal(PI);
 		BigDecimal circumferenceBD = piBD.multiply(radiusBD).multiply(new BigDecimal("2"));
-		// c=pi *r * 2, BigDecimal 的乘法方法:multiply();
+		// c=pi * r * 2, BigDecimal 的乘法方法:multiply();
+		BigDecimal areaBD = piBD.multiply(radiusBD).multiply(radiusBD);
+		// area =pi * r * r,
 		System.out.println("=====BigDecimal 精準版本====");
-
-		System.out.printf("圓周長為: %.4f\n", circumferenceBD);
-//		System.out.printf("圓面積為: %.4f\n", areaOfCircle1);
+		System.out.println("圓周長為:" + circumferenceBD.setScale(4, RoundingMode.HALF_UP));
+		// 圓周長 setscale(int ,RoundingMode)方法 (第幾位,捨入模式) (4,四捨五入)
+		System.out.println("圓面積為:" + areaBD.setScale(4, RoundingMode.HALF_UP));
+		// 圓面積
 	}
 
 }
--- src/hw2/Hw2.java
+++ src/hw2/Hw2.java
@@ -1,12 +1,23 @@
 package hw2;
-//• 請設計一隻Java程式,計算1~1000的偶數和 (2+4+6+8+...+1000)
-//• 請設計一隻Java程式,計算1~10的連乘積 (1*2*3*...*10) (用for迴圈)
-//• 請設計一隻Java程式,計算1~10的連乘積 (1*2*3*...*10) (用while迴圈)
-//• 請設計一隻Java程式,輸出結果為以下:
+//1.請建立一個TestNineNine.java程式，可輸出九九乘法表
+
+//一：使用for迴圈 + while迴圈
+//二：使用for迴圈 + do while迴圈
+//三：使用while迴圈 + do while迴圈
+
+//2.• 請設計一隻Java程式,計算1~1000的偶數和 (2+4+6+8+...+1000)
+
+//3.• 請設計一隻Java程式,計算1~10的連乘積 (1*2*3*...*10) (用for迴圈)
+
+//4.• 請設計一隻Java程式,計算1~10的連乘積 (1*2*3*...*10) (用while迴圈)
+
+//5.• 請設計一隻Java程式,輸出結果為以下:
 //1 4 9 16 25 36 49 64 81 100
-//• 阿文很熱衷大樂透 (1 ~ 49),但他不喜歡有4的數字,不論是個位數或是十位數。請設計一隻程式,
+
+//6.• 阿文很熱衷大樂透 (1 ~ 49),但他不喜歡有4的數字,不論是個位數或是十位數。請設計一隻程式,
 //輸出結果為阿文可以選擇的數字有哪些?總共有幾個?
-//• 請設計一隻Java程式,輸出結果為以下:
+
+//7.• 請設計一隻Java程式,輸出結果為以下:
 //1 2 3 4 5 6 7 8 9 10
 //1 2 3 4 5 6 7 8 9
 //1 2 3 4 5 6 7 8
@@ -17,7 +28,8 @@ package hw2;
 //1 2 3
 //1 2
 //1
-//• 請設計一隻Java程式,輸出結果為以下:
+
+//8.• 請設計一隻Java程式,輸出結果為以下:
 //A
 //BB
 //CCC
@@ -26,6 +38,37 @@ package hw2;
 //FFFFFF
 public class Hw2 {
 	public static void main(String[] args) {
+		// 1.請建立一個TestNineNine.java程式，可輸出九九乘法表
+		// 一：使用for迴圈 + while迴圈
+		System.out.println("一：使用for迴圈 + while迴圈");
+		for (int i = 1; i <= 9; i++) {
+			int j = 1;
+			while (j <= 9) {
+				System.out.println(i + "*" + j + "=" + i * j + "\t");
+				j++;
+			}
+		}
+		// 二：使用for迴圈 + do while迴圈
+		System.out.println("二：使用for迴圈 + do while迴圈");
+		for (int i = 1; i <= 9; i++) {
+			int j = 1;
+			do {
+				System.out.println(i + "*" + j + "=" + i * j + "\t");
+				j++;
+			} while (j <= 9);
+		}
+		// 一：使用for迴圈 + while迴圈
+		System.out.println("三：使用while迴圈 + do while迴圈");
+		int i0 = 1;
+		while (i0 <= 9) {
+			int j = 1;
+			do {
+				System.out.println(i0 + "*" + j + "=" + i0 * j + "\t");
+				j++;
+			} while (j <= 9);
+			System.out.println();
+			i0++;
+		}
 		// • 請設計一隻Java程式,計算1~1000的偶數和 (2+4+6+8+...+1000)
 		int sum1 = 0;
 		for (int i = 0; i <= 1000; i = i + 2) {
